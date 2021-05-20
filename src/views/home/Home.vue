5f6d97fdefcf53b110f6c3af95e76bfc1da0349d
Hanqigu <18084039758@163.com>
2021-05-20 22:05:52 +0800

    'FeatureView的封装'

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,6 +3,7 @@
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
     <home-swiper :banners="banners"></home-swiper>
     <recommend-view :recommends="recommends"></recommend-view>
+    <feature-view></feature-view>
   </div>
 </template>
 
@@ -10,6 +11,7 @@
   import NavBar from 'components/common/navbar/NavBar';
   import HomeSwiper from './childComps/HomeSwiper';
   import RecommendView from './childComps/RecommendView';
+  import FeatureView from './childComps/FeatureView';
 
   import {getHomeMultidata} from 'network/home';
 
@@ -19,6 +21,7 @@
       NavBar,
       HomeSwiper,
       RecommendView,
+      FeatureView,
     },
     data() {
       return {
@@ -38,8 +41,18 @@
 </script>
 
 <style>
+  #home {
+    padding-top: 44px;
+  }
+
   .home-nav {
     background-color: var(--color-tint);
     color: #fff;
+
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    z-index: 9;
   }
 </style>
