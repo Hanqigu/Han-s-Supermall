04708107ae6c0a59b95355032c26be04df629caa
Hanqigu <18084039758@163.com>
2021-05-26 16:58:08 +0800

    '详情页商品基本信息展示'

--- src/network/details.js
+++ src/network/details.js
@@ -9,3 +9,17 @@ export function getDetails(iid) {
     },
   });
 };
+
+// 详情页的轮播图下面的各项数据整合成一个类
+export class Goods {
+  constructor(itemInfo, columns, services) {
+    this.title = itemInfo.title;
+    this.desc = itemInfo.desc;
+    this.newPrice = itemInfo.price;
+    this.oldPrice = itemInfo.oldPrice;
+    this.discount = itemInfo.discountDesc;
+    this.columns = columns;
+    this.services = services;
+    this.realPrice = itemInfo.lowNowPrice;
+  }
+}
--- src/views/details/Details.vue
+++ src/views/details/Details.vue
@@ -2,25 +2,29 @@
   <div id="details">
     <details-nav-bar></details-nav-bar>
     <details-swiper :top-images="topImages"></details-swiper>
+    <details-base-info :goods="totalGoods"></details-base-info>
   </div>
 </template>
 
 <script>
-  import DetailsNavBar from './childComps/DetailsNavBar'
-  import DetailsSwiper from './childComps/DetailsSwiper'
+  import DetailsNavBar from './childComps/DetailsNavBar';
+  import DetailsSwiper from './childComps/DetailsSwiper';
+  import DetailsBaseInfo from './childComps/DetailsBaseInfo';
 
-  import {getDetails} from 'network/details'
+  import {getDetails, Goods} from 'network/details'
 
   export default {
     name: "Details",
     components: {
       DetailsNavBar,
       DetailsSwiper,
+      DetailsBaseInfo,
     },
     data() {
       return {
         iid: null,
         topImages: [],
+        totalGoods: {},
       };
     },
     created() {
@@ -31,8 +35,13 @@
       // 2.根据iid请求详情数据
       getDetails(this.iid).then(res => {
         // console.log(res);
+        const resData = res.result;
+
         // (1)获取顶部的图片轮播数据
-        this.topImages = res.result.itemInfo.topImages;
+        this.topImages = resData.itemInfo.topImages;
+
+        // (2)获取商品信息
+        this.totalGoods = new Goods(resData.itemInfo, resData.columns, resData.shopInfo.services);
       });
     },
   }
