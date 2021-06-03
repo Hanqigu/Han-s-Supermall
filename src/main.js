a3bf3a051ec9a6cb7a2548dfd8f1ece28a33bc82
Hanqigu <18084039758@163.com>
2021-06-03 12:20:24 +0800

    'fastclick-解决移动端300ms延迟'

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
+import FastClick from 'fastclick';
+
 import toast from 'components/common/toast';
 
 Vue.config.productionTip = false
@@ -11,6 +13,9 @@ Vue.config.productionTip = false
 Vue.prototype.$bus = new Vue();
 // 安装toast插件
 Vue.use(toast);
+
+// 解决移动端的300ms延迟
+FastClick.attach(document.body);
 
 new Vue({
   render: h => h(App),
